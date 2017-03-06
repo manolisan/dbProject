2d83a9a25be1082b4596a6c9c40f75fa357f50f5
Manolis Anastasiou <manolisan28@gmail.com>
2017-03-07 01:42:55 +0200

    Comments for patients_with_big_drugs_quantity.sql

--- queries/patients_with_big_drugs_quantity.sql
+++ queries/patients_with_big_drugs_quantity.sql
@@ -2,5 +2,5 @@ select Pa.FirstName, Pa.LastName, sum(Pr.Quantity)
 from PATIENT as Pa, PRESCRIPTION as Pr
 where Pa.PatientId = Pr.PatientId
 group by Pa.PatientId
-having sum(Pr.Quantity) > 5
+having sum(Pr.Quantity) > 5       /* Having clause is flexible. We can use '?' instead of 5. */
 order by sum(Pr.Quantity) Desc;
