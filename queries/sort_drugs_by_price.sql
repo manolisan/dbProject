45c964682dc96033d628826b142509ed092be422
toniraff <troni@protonmail.com>
2017-03-07 02:55:10 +0200

    update:sort_drugs_by_price

--- queries/sort_drugs_by_price.sql
+++ queries/sort_drugs_by_price.sql
@@ -2,7 +2,7 @@ START TRANSACTION;
 USE Prescriptions_R_X;
 
 /* order_drugs_by_price */
-SELECT d.DrugId,Name,Price
+SELECT d.DrugId,Namedrug,Price
 FROM  DRUG d
   INNER JOIN SELL s
 		ON d.DrugId=s.DrugId
