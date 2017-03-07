87b8bae51ebad201bb32bcb95a66d46f3a0edab6
Stratis Tsirtsis <stsirtsis@gmail.com>
2017-03-07 22:20:19 +0200

    Update and rename group_doctor_by_speciality_order_by_experience.sql to DONE_group_doctor_by_speciality_order_by_experience.sql

--- queries/DONE_group_doctor_by_speciality_order_by_experience.sql
+++ queries/DONE_group_doctor_by_speciality_order_by_experience.sql
@@ -1,6 +1,6 @@
 USE Prescriptions_R_X;
 
-select COUNT(Speciality) , Speciality, AVG(ExperianceYears)
+select Speciality, COUNT(Speciality), AVG(ExperianceYears)
 from DOCTOR
 group by Speciality
 order by AVG(ExperianceYears) DESC;
