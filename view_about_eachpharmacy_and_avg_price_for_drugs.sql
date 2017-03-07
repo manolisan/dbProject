66f4de08ed9135331e5451d69b7dfc4e7f66ac38
toniraff <troni@protonmail.com>
2017-03-07 22:27:29 +0200

    non updatable view

---
+++ view_about_eachpharmacy_and_avg_price_for_drugs.sql
@@ -0,0 +1,18 @@
+ USE Prescriptions_R_X; 
+
+
+CREATE VIEW vTest2 AS
+SELECT distinct P.Name,avg(Price)
+FROM SELL as S ,PHARMACY as P ,DRUG as D 
+where 
+    S.DrugId=D.DrugId
+    and
+    S.PharmacyId=P.PharmacyId
+    group by P.Name; 
+
+
+
+
+
+
+SELECT * FROM vTest2;
