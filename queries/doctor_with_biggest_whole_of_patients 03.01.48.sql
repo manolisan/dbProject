0f2e2d12671779f3ce4f9102400244c6f7ec89c0
toniraff <troni@protonmail.com>
2017-03-07 14:24:30 +0200

    DONE_doctor_with_biggest_whole_of_patients

--- queries/doctor_with_biggest_whole_of_patients 03.01.48.sql
+++ queries/doctor_with_biggest_whole_of_patients 03.01.48.sql
@@ -1,51 +1,15 @@
 USE Prescriptions_R_X;
 
-/*select FirstName, LastName
-from DOCTOR d
-  INNER JOIN PATIENT p
-		ON d.DrugId=(select p.DrugId
-			         from (SELECT MAX(patients)   
-							FROM (SELECT DoctorId,COUNT(distinct DoctorId) patients  
-							FROM PATIENT   
-							GROUP BY DoctorId)AS P) AS T);*/ 
-               
+					
 
-
-/*SELECT MAX(patients)   
-FROM (SELECT DoctorId,COUNT(distinct DoctorId) patients  
-FROM PATIENT   
-GROUP BY DoctorId) AS Plithos;*/
-
-/*select distinct FirstName, LastName
+select DOCTOR.FirstName,DOCTOR.LastName
 from DOCTOR
-where DoctorId=(select DoctorId
-				from (select max(patients)
-                
+RIGHT JOIN PATIENT ON DOCTOR.DoctorId = PATIENT.DoctorId
+group by DOCTOR.DoctorId
+order by COUNT(PATIENT.DoctorId) DESC;
                 
      
+
                 
                 
-	
-                
-                
-                
-                
-                
-                
-                */
-                
-                
-                
- 
-      
- SELECT distinct FirstName,LastName
-  from PATIENT
-  group by DoctorId
-  HAVING COUNT(DoctorId)=(SELECT max(NUM)
-						  from (select count(DoctorId) NUM from PATIENT) AS P);
-      
-      
-      
-      
-      
-      
+		
