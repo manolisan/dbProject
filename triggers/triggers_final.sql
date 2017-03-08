da750f034adc99fe75ff5eb6f154db191eef1798
Manolis Anastasiou <manolisan28@gmail.com>
2017-03-08 03:56:18 +0200

    Create triggers_final.sql

---
+++ triggers/triggers_final.sql
@@ -0,0 +1,52 @@
+DELIMITER //
+
+CREATE TRIGGER Unexperienced_doctor
+BEFORE INSERT ON DOCTOR            
+FOR EACH ROW
+BEGIN
+	IF NEW.ExperianceYears < 5 THEN SET NEW.Speciality = 'Unexperienced' ; END IF;
+END //
+
+delimiter ;
+
+
+DELIMITER //
+
+CREATE TRIGGER Check_doctor_experience
+BEFORE UPDATE ON DOCTOR            
+FOR EACH ROW
+BEGIN
+	IF NEW.ExperianceYears < 5 THEN SET NEW.Speciality = 'Unexperienced' ; END IF;
+END //
+
+delimiter ;
+
+
+DELIMITER //
+
+CREATE TRIGGER Drugs_limit
+BEFORE INSERT ON DRUG 
+FOR EACH ROW 
+BEGIN 
+	IF (select COUNT(*) from DRUG) >= 15 THEN 
+	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = "Prescription_R_X doesn't accept more than 15 different drugs!!! ";
+    END IF;
+
+END //
+
+delimiter ;
+    
+/* Dystuxws den vgainei to parakatw. Thelei procedure
+
+
+ Den petame ta palia farmaka prin eksantlithoun! Oikonomiaaaa! Ta diagrafoume otan poulhthoun! 
+	Oxi otan aplws vgei kainourgio.. 
+    
+    
+CREATE TRIGGER Reserve_old_drug_storage
+BEFORE UPDATE ON DRUG            
+FOR EACH ROW
+INSERT INTO DRUG(Name, Formula, PharmaceuticalCompanyId)
+VALUES
+(OLD.NAME, OLD.Formula, OLD.PharmaceuticalCompanyId);
+*/
