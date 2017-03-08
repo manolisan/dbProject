9add9f0efd8c2a47479ae071320cdfe040ca9d6e
Stratis Tsirtsis <stsirtsis@gmail.com>
2017-03-08 10:32:47 +0200

    Update sort_drugs_by_price.sql

--- new_ready_queries/sort_drugs_by_price.sql
+++ new_ready_queries/sort_drugs_by_price.sql
@@ -1,7 +1,7 @@
 USE Prescriptions_R_X;
 
 /* order_drugs_by_price */
-SELECT Name,Price
+SELECT Name,Price,(select Name from PHARMACY where PHARMACY.PharmacyId = s.PharmacyId)
 FROM  DRUG d
   INNER JOIN SELL s
 		ON d.DrugId=s.DrugId
