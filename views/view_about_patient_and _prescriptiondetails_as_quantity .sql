12ab1e7f28f6f70b07a752c1be53581ef4cfa340
Manolis Anastasiou <manolisan28@gmail.com>
2017-03-08 04:23:03 +0200

    Update view_about_patient_and _prescriptiondetails_as_quantity .sql

--- views/view_about_patient_and _prescriptiondetails_as_quantity .sql
+++ views/view_about_patient_and _prescriptiondetails_as_quantity .sql
@@ -1,15 +1,15 @@
+ /* View all the Prescriptions sorted by date, and all patient names! */
+ /* an thes prosthese kai farmako me to drug id */
+ 
  USE Prescriptions_R_X; 
 
 
 CREATE VIEW vTest1 AS
-SELECT  P.FirstName,P.LastName,PR.Quantity 
+SELECT  P.FirstName,P.LastName,PR.Date
 FROM  PATIENT as P INNER JOIN PRESCRIPTION as PR
 ON PR.PatientId=P.PatientId
-order by PR.Quantity;
+order by PR.Date DESC;
 
 
 
 SELECT * FROM vTest1;
-
-
-
